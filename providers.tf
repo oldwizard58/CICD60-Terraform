70bfbf0dd156787a6c686782461ed6b189100c81
oldwizard58 <solary58@yandex.ru>
2026-09-09 14:48:03 +0300

    Add files via upload

---
+++ providers.tf
@@ -0,0 +1,26 @@
+# cat ~/.aws/config 
+# [default]
+# region=ru-central1
+# cat ~/.aws/credentials 
+# [default]
+# aws_access_key_id = YCAJEK...
+# aws_secret_access_key = YCMBzZ3...
+
+
+#For terraform >=1.6<=1.8.5
+terraform {
+  required_version = ">=1.8.4"
+
+
+  required_providers {
+
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
